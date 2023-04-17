f1abfe556339db2cc854d7579458c71ee71921da
Piyush Rai <64735560+rocx320@users.noreply.github.com>
2023-04-18 00:42:41 +0530

    Changes

--- ig_prj/settings.py
+++ ig_prj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gp10',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
     }
